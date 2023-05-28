921189296dce7d37167637a45ffbec021b4624d7
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2023-05-28 18:38:58 +0200

    Use updated JLL

--- src/RustFFT.jl
+++ src/RustFFT.jl
@@ -1,9 +1,4 @@
 module RustFFT
-using JlrsCore
-if !isdefined(Main, :JlrsCore)
-    ccall(:jl_set_global, Cvoid, (Any, Any, Any), Main, :JlrsCore, JlrsCore)
-end
-
 import rustfft_jll: librustfft_path
 using JlrsCore.Wrap
 
